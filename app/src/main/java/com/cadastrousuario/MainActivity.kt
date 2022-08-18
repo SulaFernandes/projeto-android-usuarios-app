15db1d7977e7097e6345734685f0f32e104c351b
Sula Fernandes <fernandess.2015@alunos.utfpr.edu.br>
2022-08-17 22:25:21 -0300

    Atualização nas telas de login e form de cadastro

--- app/src/main/java/com/cadastrousuario/MainActivity.kt
+++ app/src/main/java/com/cadastrousuario/MainActivity.kt
@@ -4,17 +4,19 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.cadastrousuario.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val btLogin: Button = findViewById(R.id.btn_login)
-        btLogin.setOnClickListener{
-            exibirMenu()
-        }
+        binding.loginButton.setOnClickListener { exibirMenu() }
+
     }
 
     private fun exibirMenu() {
